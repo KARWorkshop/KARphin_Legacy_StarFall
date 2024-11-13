7a7ae54cf3a9ec52efac6e66e372c0fa7c9c6dbd
Sean Mott <59934015+SeanMott@users.noreply.github.com>
2024-11-12 22:50:19 -0500

    updated about page

--- Source/Core/DolphinWX/AboutDolphin.cpp
+++ Source/Core/DolphinWX/AboutDolphin.cpp
@@ -16,6 +16,8 @@
 #include "DolphinWX/AboutDolphin.h"
 #include "DolphinWX/WxUtils.h"
 
+#include "Core/KAR/Versioning.hpp"
+
 AboutDolphin::AboutDolphin(wxWindow* parent, wxWindowID id, const wxString& title,
 	const wxPoint& position, const wxSize& size, long style)
 	: wxDialog(parent, id, title, position, size, style)
@@ -23,56 +25,52 @@ AboutDolphin::AboutDolphin(wxWindow* parent, wxWindowID id, const wxString& titl
 	wxGenericStaticBitmap* const sbDolphinLogo = new wxGenericStaticBitmap(
 		this, wxID_ANY, WxUtils::LoadScaledResourceBitmap("dolphin_logo", this));
 
-	const wxString DolphinText = _("Ishiiruka - Dolphin");
-	const wxString RevisionText = scm_desc_str;
+	const wxString DolphinText = _("KARphin Tourny Edition");
 	const wxString CopyrightText =
-		_("(c) 2003-2015+ Dolphin Team. \"GameCube\" and \"Wii\" are trademarks of Nintendo. Dolphin "
+		_("(c) 2024+ Jas. \"GameCube\", \"Wii\", and \"Kirby Air Ride\" are trademarks of Nintendo. KARphin "
 			"is not affiliated with Nintendo in any way.");
 	const wxString BranchText = wxString::Format(_("Branch: %s"), scm_branch_str.c_str());
 	const wxString BranchRevText = wxString::Format(_("Revision: %s"), scm_rev_git_str.c_str());
 	const wxString CheckUpdateText = _("Check for updates: ");
 	const wxString Text =
 		_("\n"
-			"Dolphin is a free and open-source GameCube and Wii emulator.\n"
+			"KARphin is a free and open-source KAR Netplay emulator.\n"
 			"\n"
-			"This software should not be used to play games you do not legally own.\n");
+			"This software should only be used to play KAR.\n");
 	const wxString LicenseText = _("License");
 	const wxString AuthorsText = _("Authors");
 	const wxString SupportText = _("Support");
 
 	wxStaticText* const Dolphin = new wxStaticText(this, wxID_ANY, DolphinText);
-	wxStaticText* const Revision = new wxStaticText(this, wxID_ANY, RevisionText);
 
 	wxStaticText* const Copyright = new wxStaticText(this, wxID_ANY, CopyrightText);
-	wxStaticText* const Branch =
-		new wxStaticText(this, wxID_ANY, BranchText + "\n" + BranchRevText + "\n");
+	wxStaticText* const Version =
+	    new wxStaticText(this, wxID_ANY,
+	                     std::string("Version: ") + KAR_TOURNY_EDITION_VER_MAJOR + "-" + KAR_TOURNY_EDITION_VER_MINOR + "-" +
+	                         KAR_TOURNY_EDITION_VER_HOT_FIX + "\n");
 	wxStaticText* const Message = new wxStaticText(this, wxID_ANY, Text);
 	wxStaticText* const UpdateText = new wxStaticText(this, wxID_ANY, CheckUpdateText);
 	wxStaticText* const FirstSpacer = new wxStaticText(this, wxID_ANY, "  |  ");
 	wxStaticText* const SecondSpacer = new wxStaticText(this, wxID_ANY, "  |  ");
-	wxHyperlinkCtrl* const Download = new wxHyperlinkCtrl(this, wxID_ANY, "dolphin-emu.org/download",
-		"https://dolphin-emu.org/download/");
+	wxHyperlinkCtrl* const Download = new wxHyperlinkCtrl(this, wxID_ANY, "KAR Workshop/download",
+		"https://karworkshop.sean-mott.com/#download");
 	wxHyperlinkCtrl* const License =
 		new wxHyperlinkCtrl(this, wxID_ANY, LicenseText,
 			"https://github.com/dolphin-emu/dolphin/blob/master/license.txt");
 	wxHyperlinkCtrl* const Authors = new wxHyperlinkCtrl(
 		this, wxID_ANY, AuthorsText, "https://github.com/dolphin-emu/dolphin/graphs/contributors");
 	wxHyperlinkCtrl* const Support =
-		new wxHyperlinkCtrl(this, wxID_ANY, SupportText, "https://forums.dolphin-emu.org/");
+		new wxHyperlinkCtrl(this, wxID_ANY, SupportText, "https://discord.com/channels/266095631804792835/384118998175973377");
 
 	wxFont DolphinFont = Dolphin->GetFont();
-	wxFont RevisionFont = Revision->GetFont();
+	wxFont RevisionFont = Version->GetFont();
 	wxFont CopyrightFont = Copyright->GetFont();
-	wxFont BranchFont = Branch->GetFont();
 
 	DolphinFont.SetPointSize(36);
 	Dolphin->SetFont(DolphinFont);
 
 	RevisionFont.SetWeight(wxFONTWEIGHT_BOLD);
-	Revision->SetFont(RevisionFont);
-
-	BranchFont.SetPointSize(7);
-	Branch->SetFont(BranchFont);
+	Version->SetFont(RevisionFont);
 
 	CopyrightFont.SetPointSize(7);
 	Copyright->SetFont(CopyrightFont);
@@ -101,9 +99,8 @@ AboutDolphin::AboutDolphin(wxWindow* parent, wxWindowID id, const wxString& titl
 	wxBoxSizer* const sInfo = new wxBoxSizer(wxVERTICAL);
 	sInfo->Add(Dolphin);
 	sInfo->AddSpacer(space5);
-	sInfo->Add(Revision);
+	sInfo->Add(Version);
 	sInfo->AddSpacer(space10);
-	sInfo->Add(Branch);
 	sInfo->Add(sCheckUpdates);
 	sInfo->Add(Message);
 	sInfo->Add(sLinks);
